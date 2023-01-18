f8cb3ceb60146e21a7f4f5384a0960899fbd038a
Zhi-Hahn See <zhihahnsee@gmail.com>
2023-01-17 22:04:04 -0800

    Init styling + JSX for react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.png';
+import linkedin from './images/linkedin.png';
+import github from './images/github.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "homepage">
+      <nav>
+        <a href = "./App.js" className = "logo"><img className = "logopng" src = {logo} alt = "logo"></img></a>
+        <ul className = "links">
+          <li><a href = "./App.js">Home</a></li>
+          <li><a href = "./Packages.js">My Packages</a></li>
+          <li><a href = "./About.js">About</a></li>
+          <li><a href = "./Contact.js">Contact</a></li>
+        </ul>
+        <ul className = "social-links">
+          <li><a href = "https://github.com/zhihahnsee"><img className = "github" src = {github} alt = "GitHub"></img></a></li>
+          <li><a href = "https://www.linkedin.com/in/zhi-hahn-see/"><img className = "linkedin" src = {linkedin} alt = "LinkedIn"></img></a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
