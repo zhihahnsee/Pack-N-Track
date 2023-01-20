b9ecd5335fd4f6fdc7aa388b1e3c5e10bbf24f55
Zhi-Hahn See <zhihahnsee@gmail.com>
2023-01-19 23:39:51 -0800

    Create files for components, organize for routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import './App.css';
-import logo from './images/logo.png';
-import linkedin from './images/linkedin.png';
-import github from './images/github.png';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Packages from "./Components/Packages";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <div className = "homepage">
-      <nav>
-        <a href = "./App.js" className = "logo"><img className = "logopng" src = {logo} alt = "logo"></img></a>
-        <ul className = "links">
-          <li><a href = "./App.js">Home</a></li>
-          <li><a href = "./Packages.js">My Packages</a></li>
-          <li><a href = "./About.js">About</a></li>
-          <li><a href = "./Contact.js">Contact</a></li>
-        </ul>
-        <ul className = "social-links">
-          <li><a href = "https://github.com/zhihahnsee"><img className = "github" src = {github} alt = "GitHub"></img></a></li>
-          <li><a href = "https://www.linkedin.com/in/zhi-hahn-see/"><img className = "linkedin" src = {linkedin} alt = "LinkedIn"></img></a></li>
-        </ul>
-      </nav>
-      <div className = "search-outer-rectangle"></div>
-      <div className = "pre-search-text">
-        <h1>Track your packages!</h1>
-        <p>Quick and easy package tracking <br/> By Pack N Track</p>
-      </div>
-      <div className = "tracking-number-input">
-        <input type = "text" name = "tracking-number" placeholder = "Input your tracking number..." />
-        <button type = "button">Go!</button>
-      </div>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path = "/" element = {<Home />} />
+          <Route exact path = "/packages" element = {<Packages />} />
+          <Route exact path = "/about" element = {<About />} />
+          <Route exact path = "/contact" element = {<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
